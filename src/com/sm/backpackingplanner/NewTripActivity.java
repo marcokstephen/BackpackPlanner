b6f94b5f9227ac91ed0df615fd7167cad03ebcf6
Ashley <eacortez2@pipeline.sbcc.edu>
2014-10-04 09:36:07 -0700

    data picker , search bar

--- src/com/sm/backpackingplanner/NewTripActivity.java
+++ src/com/sm/backpackingplanner/NewTripActivity.java
@@ -1,16 +1,20 @@
 package com.sm.backpackingplanner;
 
-import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class NewTripActivity extends Activity {
+public class NewTripActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_trip);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -25,10 +29,19 @@ public class NewTripActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		if (item.getItemId() == android.R.id.home){
+			finish();
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void DataPicker(View v){
+//		Intent intent = new Intent(this,DatePickerFragment.class);
+//		startActivity(intent);
+		
+	    DialogFragment newFragment = new DatePickerFragment();
+	    newFragment.show(getFragmentManager(), "datePicker");
+	}
+	
+	
 }
---
+++ src/com/sm/backpackingplanner/SearchableLocation.java
@@ -0,0 +1,5 @@
+package com.sm.backpackingplanner;
+
+public class SearchableLocation {
+
+}
