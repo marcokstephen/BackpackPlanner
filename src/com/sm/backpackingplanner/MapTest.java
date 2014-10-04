6adfc122dc4d77246fb6e26fa7e760af23c3d423
Eduardo Flores <i.m.dragon@gmail.com>
2014-10-04 15:33:14 -0700

    Map working now

--- src/com/sm/backpackingplanner/MapTest.java
+++ src/com/sm/backpackingplanner/MapTest.java
@@ -1,13 +1,29 @@
 package com.sm.backpackingplanner;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MapTest extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_test);
-    }
+public class MapTest extends FragmentActivity {
+
+	private GoogleMap map;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_test);
+
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		map.setMyLocationEnabled(true);
+		
+//		map.getMyLocation();
+//		CameraPosition cameraPosition = new CameraPosition.Builder().target(
+//                new LatLng(myLoc.getLatitude(), myLoc.getLongitude())).zoom(12).build();
+// 
+//map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+	}
+
 }
