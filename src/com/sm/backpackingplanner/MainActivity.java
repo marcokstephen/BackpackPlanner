24f7e2d8156f7fdb205ccf4035ee55e51dfe34e5
marcokstephen <marcok.stephen@gmail.com>
2014-10-04 02:41:38 -0700

    Adding to mainactivity

--- src/com/sm/backpackingplanner/MainActivity.java
+++ src/com/sm/backpackingplanner/MainActivity.java
@@ -1,13 +1,21 @@
 package com.sm.backpackingplanner;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
@@ -28,8 +36,10 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+        
+        ListView tripListView = (ListView)findViewById(R.id.tripListView);
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +58,49 @@ public class MainActivity extends Activity {
         	return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public class TripListAdapter extends BaseAdapter {
+    	
+    	private LayoutInflater myLayoutInflater;
+    	private JSONArray myJsonArray;
+    	
+    	public TripListAdapter(Context ctxt, String jsonTripList){
+    		myLayoutInflater = (LayoutInflater) ctxt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    		try {
+    			myJsonArray = new JSONArray(jsonTripList);
+    		} catch (JSONException e){
+    			e.printStackTrace();
+    		}
+    	}
+    	
+		@Override
+		public int getCount() {
+			return myJsonArray.length();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			try {
+				return myJsonArray.get(arg0);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int arg0, View view, ViewGroup arg2) {
+			if (view == null){
+				view = myLayoutInflater.inflate(R.layout.trip_list_view_element, null);
+			}
+			return null;
+		}
+    	
     }
 }
